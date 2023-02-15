25c04f14414c0f5a6d91277f962f729bbc9fc1fc
navnit.amcodr <navnit.amcodr@gmail.com>
2023-02-15 15:40:57 +0530

    various changes and add hour log delete functionality

--- app/Lesson.php
+++ app/Lesson.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Lesson extends Model
 {
     protected $table = 'lessons';
+
+    public function lessonHourLogs()
+    {
+        return $this->hasOne(LessonLog::class, 'lesson_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'updated_by');
+    }
 }
--- app/LessonLog.php
+++ app/LessonLog.php
@@ -10,4 +10,14 @@ class LessonLog extends Model
     use SoftDeletes;
     
     protected $table = 'lesson_hour_logs';
+
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'created_by');
+    }
 }
